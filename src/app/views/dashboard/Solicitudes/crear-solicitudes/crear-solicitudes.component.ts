ce37676867b20ee37ee6b11c89d6d6e2a2207451
Ernesto Salazar <nethojs29@gmail.com>
2017-09-12 18:28:00 -0700

    Preparando forms

--- src/app/views/dashboard/Solicitudes/crear-solicitudes/crear-solicitudes.component.ts
+++ src/app/views/dashboard/Solicitudes/crear-solicitudes/crear-solicitudes.component.ts
@@ -10,10 +10,46 @@ import { Class, Component, OnInit, NgModule } from '@angular/core';
 
 export class CrearSolicitudesComponent implements OnInit {
 public solicitud_moral: boolean;
+public datos = {
+  solicitud : {
+    amount : 0,
+    start_date : Date.now(),
+    location : "",
+    term : 0,
+    name : ""
+  },
+  avales : {
+    name : "",
+    lastname : "",
+    RFC : "",
+    CURP : "",
+    birthdate : Date.now(),
+    country : "",
+    nationality : "",
+    email : "",
+    fiel : "",
+    address : "",
+    phone_number : "",
+    marital_status : "",
+    marital_regimen : "",
+    relationship : "",
+    workplace : "",
+    work_phone : "",
+    position : "",
+    join_date : Date.now()
 
+
+  },
+  archivos: {
+    comprobante : "",
+    estadoCuenta : "",
+    RFC : "",
+    contrato : ""
+  }
+}
 constructor() {
   this.solicitud_moral = false;
-
+  
 }
 
 evento_solicitud(valor) {
--- src/app/views/dashboard/Solicitudes/crear-solicitudes/physical-person/aval-p/aval-p.component.ts
+++ src/app/views/dashboard/Solicitudes/crear-solicitudes/physical-person/aval-p/aval-p.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aval-p.component.scss']
 })
 export class AvalPComponent implements OnInit {
+  avales = {
+    name : "",
+    lastname : "",
+    RFC : "",
+    CURP : "",
+    birthdate : Date.now(),
+    country : "",
+    nationality : "",
+    email : "",
+    fiel : "",
+    address : "",
+    phone_number : "",
+    marital_status : "",
+    marital_regimen : "",
+    relationship : "",
+    workplace : "",
+    work_phone : "",
+    position : "",
+    join_date : Date.now()
 
+
+  }
   constructor() { }
 
   ngOnInit() {
