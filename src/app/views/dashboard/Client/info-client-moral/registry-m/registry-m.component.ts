6d21d2b0c4d88deb794f722006ad0e07e0494a4e
Ernesto Salazar <nethojs29@gmail.com>
2017-09-25 16:06:15 -0700

    Cambios al login

--- src/app/views/dashboard/Client/info-client-moral/registry-m/registry-m.component.ts
+++ src/app/views/dashboard/Client/info-client-moral/registry-m/registry-m.component.ts
@@ -12,7 +12,7 @@ import { PatternValidator} from '@angular/forms';
 export class RegistryMComponent implements OnInit {
 
   submitted = false;
-  public RFCPattern = '[A-Z,Ñ,&]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[A-Z|\d]{3}';
+  public RFCPattern = '[A-Z,Ñ,&]{3}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[A-Z|\d]{3}';
   public FIELPattern = '[0-9]{18}([0-9]?){2}';
   public emailPattern = '(([^<>()\\[\\]\\\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))';
   public phonePattern = '[0-9]{1,10}';
--- src/app/views/dashboard/Client/info-client-physic/registry-p/registry-p.component.ts
+++ src/app/views/dashboard/Client/info-client-physic/registry-p/registry-p.component.ts
@@ -1,10 +1,12 @@
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { RegistryP } from './m-registry-p';
 import { Component, OnInit } from '@angular/core';
+import { PatternValidator} from '@angular/forms';
 
 @Component({
   selector: 'app-registry-p',
   templateUrl: './registry-p.component.html',
+  providers: [PatternValidator],
   styleUrls: ['./registry-p.component.scss']
 })
 export class RegistryPComponent implements OnInit {
@@ -13,7 +15,10 @@ export class RegistryPComponent implements OnInit {
 
   model: RegistryP = new RegistryP();
 
-
+  public RFCPattern = '[A-Z,Ñ,&]{4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[A-Z|\d]{3}';
+  public FIELPattern = '[0-9]{18}([0-9]?){2}';
+  public emailPattern = '(([^<>()\\[\\]\\\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))';
+  public phonePattern = '[0-9]{1,10}';
   ngOnInit() {
   }
 
