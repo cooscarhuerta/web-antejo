f6a1616ded764b6f883442c33053531173603dd7
COT <oscarhuerttta@gmail.com>
2017-10-05 12:59:30 -0700

    Redireccionamiento de detalle

--- src/app/views/dashboard/Solicitudes/ver-solicitudes/ver-solicitudes.component.ts
+++ src/app/views/dashboard/Solicitudes/ver-solicitudes/ver-solicitudes.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
@@ -10,29 +11,24 @@ import { SweetAlertService } from 'ng2-sweetalert2';
   styleUrls: ['./ver-solicitudes.component.scss']
 })
 export class VerSolicitudesComponent implements OnInit {
-  appsArray = ['1'];
+  appsArray = [];
   dataFinishedLoading = false;
-  constructor(private http: HttpClient, private sweetAlert : SweetAlertService) {
+  constructor(private route: Router, private http: HttpClient, private sweetAlert: SweetAlertService) {
 
   }
 
   ngOnInit() {
-    this.http.get(
-      '/Clients/Clientes/show/' + localStorage.getItem('userId') + '/Wallet').subscribe(data => {
-    // Read the result field from the JSON response.
-    if (data['applications'].length == 0) {
-      console.log('Applications query failed');
-      this.sweetAlert.swal("Aviso","No tiene aplicaciones registradas.","warning");
-      this.dataFinishedLoading = true;
+    this.http.get('/Clients/Clientes/show/' + localStorage.getItem('userId') + '/Wallet').subscribe(data => {
+        // Read the result field from the JSON response.
+        if (data['applications'].length === 0) {
+          console.log('Applications query failed');
+          this.sweetAlert.swal('Aviso', 'No tiene aplicaciones registradas.', 'warning');
+          this.dataFinishedLoading = true;
 
-    }else {
-      console.log(data['applications']);
-      this.appsArray = data['applications'];
-      console.log('Applications get!');
-      this.dataFinishedLoading = true;
-    }
-    console.log(data);
-    });
+        } else {
+          this.appsArray = data['applications'];
+          this.dataFinishedLoading = true;
+        }
+      });
   }
-
 }
